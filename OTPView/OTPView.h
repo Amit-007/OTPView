6558367aa336c3dbbca75c0f00410e0bc36ff785
Majumdar, Amit (US - Bengaluru) <ammajumdar@deloitte.com>
2020-03-30 16:48:18 +0530

    Initial Commit

---
+++ OTPView/OTPView.h
@@ -0,0 +1,19 @@
+//
+//  OTPView.h
+//  OTPView
+//
+//  Created by Majumdar, Amit on 30/03/20.
+//  Copyright © 2020 Stride[247]. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OTPView.
+FOUNDATION_EXPORT double OTPViewVersionNumber;
+
+//! Project version string for OTPView.
+FOUNDATION_EXPORT const unsigned char OTPViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTPView/PublicHeader.h>
+
+
---
+++ OTPViewExampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OTPViewExampleProject
+//
+//  Created by Majumdar, Amit on 30/03/20.
+//  Copyright © 2020 Stride[247]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
